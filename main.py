30e26d1ccac68d7b532c1217f4a63256c4326b43
volde-morticia <107201401+volde-morticia@users.noreply.github.com>
2022-07-09 14:07:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
 import random
 from colorama import Fore, Style
 from os import system
-from time import sleep
 from PyDictionary import PyDictionary
-
-
-words = ["S H A K Y", "C L U M P", "S L A S H", "C L A C K", "S C I O N", "B I G O T", "C R U E L", "S Q U A D", "S H R U G", "C R A Z E", "S A U C E", "S T U N K", "P U L S E", "I N L A Y", "G L E A N", "E D G E D", "H A V O C", "E A V E S", "D A N C E", "S P O R E", "S P I T E", "T R U C E", "E X I S T", "Q U I C K", "D R E A M", "P R O X Y", "S O B E R"]
-
+from content import words, title, dashes
 
 # to clear the console
 def clear():
   _ = system('clear')
-  print('''
-                    _ _     
-__ __ _____ _ _ __| | |___ 
-\ V  V / _ \ '_/ _` | / -_)
- \_/\_/\___/_| \__,_|_\___|
-               
-''')
+  print(title)
 
+# to play game again
+def play_again():
+  again = input("Try again with a different word?\nType YES or NO\n").upper()
+  if again == "YES":
+    print()
+    print("Okay, I'll try to go easy on you this time.\n")
+    clear()
+    print(dashes)
+    game()
+  else:
+    clear()
+    print("Thanks for playing! <3\n\nFollow me on Github:\nhttps://github.com/volde-morticia\n\n")
+  
+  
+# game end messages
+def endgame(win, tries_left, word):
+  if win == True:
+    print("That's the word! Good job, you\nare amazing, you know all the words,\nhere's your star ⭐\n")
+  else:
+    if tries_left == 0:
+      print(f"You lost! The word is {word}.\n")
+  play_again()
+  
+# to print the full list of words
+def print_list(blanks):
+  for a in range(0,6):
+    for b in range(0,5):
+      print(blanks[a][b] + " ", end = '')
+      print(Style.RESET_ALL, end = '')
+    print()
+  print("\n")
+  return
 
-# function to check whether it's a real word
+# to check whether input is a real word
 def check_if_real(guess):
     dictionary = PyDictionary()
     if dictionary.meaning(guess,True) is None:
@@ -28,23 +50,21 @@ def check_if_real(guess):
     else:
       return True
       
-
-# function to accept the input
+# to accept the input and check if it's valid
 def word_input ():
   x = 0
   while x == 0:
     guess = input("Enter your guess.\n").upper()
-    print()
+    print("\nProcessing...\n\n")
     length = len(guess)
     if length != 5:
-      print("You can't enter a word that isn't\nexactly 5 letters! smh\n")
+      print("You can't enter a word that isn't\nexactly 5 letters!\n")
     else:
       if check_if_real(guess) == False:
-        print("That's not a real word. smh.\n")
+        print("That's not a real word.\n")
       else:
         x = 1
         return(guess)
-
 
 def game():
   # creating a blank list
@@ -53,7 +73,9 @@ def game():
   for i in range (0,6):
     blanks.append(blank)
 
-  # selecting a random word, splitting the letters, joining the letters
+  # selecting a random word
+  # splitting the letters of the word into a list
+  # joining the letters together to get a whole word
   word_space = random.choice(words)
   word_list = word_space.split(" ")
   word = ""
@@ -83,49 +105,55 @@ def game():
 
     clear()
     
-    # printing the full list
-    for a in range(0,6):
-      for b in range(0,5):
-        print(blanks[a][b] + " ", end = '')
-        print(Style.RESET_ALL, end = '')
-      print()
-    print("\n")
+    print_list(blanks)
 
-  # winning & losing prompt
-  if win == True:
-    print("That's the word! Wow, I kinda wasn't\nexpecting you to win. Good job, you\nare amazing, you know all the words,\nhere's your star ⭐\n")
-  else:
-    if tries_left == 0:
-      print(f"The word is {word}.\nLooks like you suck at this game!\nDo you even know any words?\nIt's just five letters it's really\nnot that hard.\n")
+  endgame(win, tries_left, word)
 
-  again = input("Try again with a different word?\nType YES or NO\n").upper()
-  if again == "YES":
-    print()
-    print("Okay, I'll try to go easy on you this time.\n")
+# example to explain the intructions
+def example():
+  ex_word = "WATER"
+  ex_guess = "RAYON"
+  ex_guess_list = []
+  
+  for n in range(0,5):
+    c = ex_guess[n]
+    if c in ex_word:
+      if c == ex_word[n]:
+        c2 = Fore.GREEN + c
+      else:
+        c2 = Fore.YELLOW + c
+    else:
+      c2 = c
+    ex_guess_list.append(c2)
+
+  print(f"EXAMPLE:\n\nLet's say the word you have to guess is: {ex_word}\n\nAnd a guess that you make is: {ex_guess}\n")
+        
+  for i in range(0,5):
+    print(ex_guess_list[i] + " ", end = '')
+    print(Style.RESET_ALL, end = '')
+
+  print(f"\n\nHere, the letter 'R' is yellow,\nbecause in the word '{ex_word}',\nthere is an 'R', but not at the\nsame position as '{ex_guess}'.\nThe letter 'A' is green, because\nit exists in the word '{ex_word}' at the\nsame position as '{ex_guess}'\n\n")
+  
+  option = input("Enter ST to start the game.\n\n").upper()
+  if option == "ST":
     clear()
-    print('''_ _ _ _ _
-_ _ _ _ _
-_ _ _ _ _
-_ _ _ _ _
-_ _ _ _ _
-_ _ _ _ _
-
-''')
+    print(dashes)
     game()
-  else:
-    clear()
-    print("Thanks for playing! <3\n\nFollow me on Github:\nhttps://github.com/volde-morticia\n\nIf you have any feedback\nlet me know!")
 
-    
+  else:
+    print("Invalid input.")
+    options()
+  
 # options to either start the game or to view instructions
 def options():
   option = input('''
 ?      -  how to play
-start  -  start the game
+st  -  start the game
 
 ''').upper()
   print()
 
+  # game instructions
   if option == "?":
     green = Fore.GREEN + "green"
     yellow = Fore.YELLOW + "yellow"
@@ -137,35 +165,20 @@ start  -  start the game
     print(Style.RESET_ALL, end = '')
     print(",\nit exists in the word at the same\nspot.")
     print("If a letter is neither of those\ncolours, it doesn't exist in the\nword.\n")
-    option = input("Enter START to start the game.\n\n").upper()
+    option = input("Enter EX to view an example, OR\nEnter ST to start the game.\n\n").upper()
 
-  if option == "START":
+  if option == "EX":
     clear()
-    print('''_ _ _ _ _
-_ _ _ _ _
-_ _ _ _ _
-_ _ _ _ _
-_ _ _ _ _
-_ _ _ _ _
-
-''')
+    example()
+  elif option == "ST":
+    clear()
+    print(dashes)
     game()
 
-  if option != "?" and option != "START":
-    print("Invalid input. smh.")
+  if option != "?" and option != "ST" and option != "EX":
+    print("Invalid input.")
     options()
 
-    
 # title
-print('''
-                    _ _     
-__ __ _____ _ _ __| | |___ 
-\ V  V / _ \ '_/ _` | / -_)
- \_/\_/\___/_| \__,_|_\___|
-               
-''')
+print(title)
 options()
-
-
-# to add:
-# if the letter exists only once, highlight it only once and not twice
